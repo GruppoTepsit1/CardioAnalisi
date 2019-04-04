3b90412b9b1e10ff85644e0ff724dd443c397ba9
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-04 10:13:51 +0200

    completati i primi 2 punti

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -9,11 +9,38 @@ namespace CardioLibrary
 {
     public static class DataCardio
     {
-        public static double fequenza1(double età)
-        { 
+        public static double frequenza1(double età)
+        {
 
-            double risposta =  220 - età ;
-          
+            double risposta = 220 - età;
+
+            return risposta;
+        }
+        public static bool Consigliata1(double frequenzamax)
+        {
+            double set = 0, nov = 0, b =140;
+            set = frequenzamax / 100 * 70;
+            nov = frequenzamax / 100 * 90;
+            bool risposta=false;
+            if(b>set && b < nov)
+            {
+               risposta = true;
+            }
+            
+            return risposta;
+        }
+        public static string Normale1(double frequenza)
+        {
+            string risposta = "Normale";
+            if (frequenza < 60)
+            {
+                risposta = "Bradicardia";
+            }
+            if (frequenza > 100)
+            {
+                risposta = "Tachicardia";
+            }
+            
             return risposta;
         }
     }
--- DataCardio_Tests/CardioTests.cs
+++ DataCardio_Tests/CardioTests.cs
@@ -7,14 +7,41 @@ namespace DataCardio_Tests
     [TestClass]
     public class CardioTests
     {
-         
+         //test per verificare la frequenza ideale in base all'età
         [TestMethod]
-        public void TestMethod1()
+        public void TestFrequenza1()
         {
             double età = 20, frequenza=200;
 
-            double i = DataCardio.fequenza1(età);
+            double i = DataCardio.frequenza1(età);
             Assert.AreEqual(frequenza, i);
         }
+        [TestMethod]
+        public void TestFrequenza2()
+        {
+            double età = 50, frequenza = 170;
+
+            double i = DataCardio.frequenza1(età);
+            Assert.AreEqual(frequenza, i);
+        }
+        //test per verificare la frequanza ideale, compresa tra min e max
+        [TestMethod]
+        public void TestIdeale1()
+        {
+            double  frequenzamax = 170;
+            bool RespAttesa = true;
+           bool i = DataCardio.Consigliata1(frequenzamax);
+            Assert.AreEqual( RespAttesa, i);
+        }
+        //test per verificare il battito a riposo
+        [TestMethod]
+        public void Testriposo1()
+        {
+            double  frequenza = 200;
+            string risposta = "Tachicardia";
+            string i = DataCardio.Normale1(frequenza);
+            Assert.AreEqual(risposta, i);
+        }
+
     }
 }
