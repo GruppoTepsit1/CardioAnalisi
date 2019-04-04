45112c5fbd07c3bc51f81e5b532c47e4c8a34e52
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-04-04 10:16:35 +0200

    si ha trovato la frequenza massima

--- DataCardio_Tests/CardioTests.cs
+++ DataCardio_Tests/CardioTests.cs
@@ -7,9 +7,38 @@ namespace DataCardio_Tests
     [TestClass]
     public class CardioTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        
+            [TestMethod]
+        public void TestFrequenzaMassima1()
         {
+            double eta = 20;
+            string rispattesa = "200", risp = DataCardio.cardioanalisi(eta);
+            Assert.AreEqual(risp, rispattesa);
+
+
+
+        }
+        [TestMethod]
+        public void TestFrequenzaMassima2()
+        {
+            double eta = 40;
+            string rispattesa = "180", risp = DataCardio.cardioanalisi(eta);
+            Assert.AreEqual(risp, rispattesa);
+
+
+
+        }
+        [TestMethod]
+        public void TestFrequenzaMassima3()
+        {
+            double eta = -3;
+
+            string rispattesa = "impossibile", risp = DataCardio.cardioanalisi(eta);
+            Assert.AreEqual(risp, rispattesa);
+
+
+
         }
     }
+    
 }
