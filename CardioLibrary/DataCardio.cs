7a2d53140f140cc0d5ddb6290e0b5721b320b888
Filippo <f.izum02@gmail.com>
2019-05-01 14:54:11 +0200

    Progetto terminato e funzionante

--- CardioLibrary/DataCardio.cs
+++ CardioLibrary/DataCardio.cs
@@ -9,5 +9,143 @@ namespace CardioLibrary
 {
     public static class DataCardio
     {
+        //Metodo per calcolare la frequenza massima
+        public static double Frequenza(double eta)
+        {
+            double risposta = 220 - eta;
+            return risposta;
+        }
+
+        //Metodo di calcolo della frequenza massima ideale in allenamento
+        public static double CalcoloMaxFrequenzaIdeale(double anni)
+        {
+            double frequenza = 220;
+            frequenza = frequenza - anni;
+            double ideale = (frequenza / 100) * 90;
+            return ideale;
+        }
+
+        //Metodo di calcolo della frequenza minima ideale in allenamento
+        public static double CalcoloMinFrequenzaIdeale(double anni)
+        {
+            double frequenza = 220;
+            frequenza = frequenza - anni;
+            double ideale = (frequenza / 100) * 70;
+            return ideale;
+        }
+
+        ////Metodo per interpretare i valori della frequenza cardiaca a riposo
+        public static string FrequenzaRiposo(double frequenza)
+        {
+            string risposta = "Normale";
+            if (frequenza < 60)
+            {
+                risposta = "Bradicardia";
+            }
+            if (frequenza > 100)
+            {
+                risposta = "Tachicardia";
+            }
+            return risposta;
+        }
+
+        //Metodo di calcolo delle calorie bruciate in un allenamento
+        public static string Esercizi(double frequenza, double peso, double età, double tempo, string sesso)
+        {
+            double Calorie = 0;
+            if (sesso == "M")
+            {
+                double caloriebruciate;
+                double T = Math.Round(tempo / 4.184, 4);
+
+                caloriebruciate = ((età * 0.2017) + (peso * 0.199) + (frequenza * 0.6309) - 55.0969) * T;
+                Calorie = caloriebruciate;
+            }
+            else
+            {
+                double caloriebruciate;
+                double T = Math.Round(tempo / 4.184, 4);
+
+                caloriebruciate = ((età * 0.074) + (peso * 0.126) + (frequenza * 0.4472) - 20.4022) * T;
+                Calorie = caloriebruciate;
+            }
+            string i = Convert.ToString(Math.Round(Calorie, 4));
+            return i;
+        }
+
+        //Metodo che calcola la spesa energetica nella corsa e nella camminata
+        public static string MovCorsa(double peso, double chilometri)
+        {
+            string Calorie = "";
+            double SpesaEnergetica = 0;
+            SpesaEnergetica = 0.9 * chilometri * peso;
+            Calorie = Convert.ToString(SpesaEnergetica);
+
+            return Calorie;
+        }
+
+        public static string MovCamminata(double peso, double chilometri)
+        {
+            string Calorie = "";
+            double SpesaEnergetica = 0;
+            SpesaEnergetica = 0.5 * chilometri * peso;
+            Calorie = Convert.ToString(SpesaEnergetica);
+
+            return Calorie;
+        }
+
+        //Metodo di calcolo della media giornaliera dei battiti cardiaci
+        public static double MediaBattiti(int[] BattitiGiornalieri)
+        {
+            int somma = 0;
+            for (int i = 0; i < BattitiGiornalieri.Length; i++)
+            {
+                somma += BattitiGiornalieri[i];
+            }
+
+            double media = somma / BattitiGiornalieri.Length;
+            return media;
+        }
+
+        //Metodo di calcolo del battito a riposo in un array
+        public static double BattitoRiposo(int[] Battiti)
+        {
+            int min = Battiti[0];
+            for (int i = 0; i < Battiti.Length; i++)
+            {
+                if (min > Battiti[i])
+                {
+                    min = Battiti[i];
+                }
+            }
+            return min;
+        }
+
+        //Metodo di calcolo della variabilità del battito caridaco
+        public static double VariazioneBattito(int[] Battiti)
+        {
+            int min = Battiti[0], max = Battiti[0];
+            for (int i = 0; i < Battiti.Length; i++)
+            {
+                if (min > Battiti[i])
+                {
+                    min = Battiti[i];
+                }
+
+                if (max < Battiti[i])
+                {
+                    max = Battiti[i];
+                }
+            }
+            int variazione = max - min;
+            return variazione;
+        }
+
+        //Metodo di riordinamento dell'array
+        public static int[] Riordinamento(int[] Battiti)
+        {
+            Array.Sort(Battiti);
+            return Battiti;
+        }
     }
 }
--- DataCardio_Tests/CardioTests.cs
+++ DataCardio_Tests/CardioTests.cs
@@ -7,12 +7,239 @@ namespace DataCardio_Tests
     [TestClass]
     public class CardioTests
     {
+        //Test per verificare la frequenza massima
         [TestMethod]
-        public static int CalcoloFrequenzaCardiaca(int anni)
+        public void TestFrequenza1()
         {
-            int frequenza = 220;
-            frequenza = frequenza - anni;
-            return frequenza;
+            double età = 20, frequenza = 200;
+
+            double i = DataCardio.Frequenza(età);
+            Assert.AreEqual(frequenza, i);
+        }
+
+        [TestMethod]
+        public void TestFrequenza2()
+        {
+            double età = 50, frequenza = 170;
+
+            double i = DataCardio.Frequenza(età);
+            Assert.AreEqual(frequenza, i);
+        }
+
+        //Tests per verificare lafrequenza massima e minima ideale durante un'allenamnto
+        [TestMethod]
+        public void TestMaxFrequenzaIdeale()
+        {
+            double anni = 24;
+            double maxfrequenzaideale = 176.4;
+            double finalvalue = 0;
+            finalvalue = DataCardio.CalcoloMaxFrequenzaIdeale(anni);
+            Assert.AreEqual(maxfrequenzaideale, finalvalue);
+        }
+
+        [TestMethod]
+        public void TestMinFrequenzaIdeale()
+        {
+            double anni = 24;
+            double minfrequenzaideale = 137.2;
+
+            double finalvalue = 0;
+            finalvalue = DataCardio.CalcoloMinFrequenzaIdeale(anni);
+            Assert.AreEqual(minfrequenzaideale, finalvalue);
+        }
+
+
+        //test per verificare il battito a riposo
+        [TestMethod]
+        public void Testriposo1()
+        {
+            double frequenza = 200;
+            string risposta = "Tachicardia";
+            string i = DataCardio.FrequenzaRiposo(frequenza);
+            Assert.AreEqual(risposta, i);
+        }
+
+        [TestMethod]
+        public void Testriposo2()
+        {
+            double frequenza = 100;
+            string risposta = "Normale";
+            string i = DataCardio.FrequenzaRiposo(frequenza);
+            Assert.AreEqual(risposta, i);
+        }
+
+        [TestMethod]
+        public void Testriposo3()
+        {
+            double frequenza = 54;
+            string risposta = "Bradicardia";
+            string i = DataCardio.FrequenzaRiposo(frequenza);
+            Assert.AreEqual(risposta, i);
+        }
+
+        //Test di verifica delle calorie bruciate durante un allenamento
+        [TestMethod]
+        public void TestCalorieBruciate1()
+        {
+            double frequenza = 176, peso = 80, eta = 24, tempo = 30;
+            string RispAsp = "549,9708", sesso = "M";
+            string finalvalue = DataCardio.Esercizi(frequenza, peso, eta, tempo, sesso);
+
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        [TestMethod]
+        public void TestCalorieBruciate2()
+        {
+            double frequenza = 182, peso = 65, eta = 17, tempo = 30;
+            string RispAsp = "545,5863", sesso = "M";
+            string finalvalue = DataCardio.Esercizi(frequenza, peso, eta, tempo, sesso);
+
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        [TestMethod]
+        public void TestCalorieBruciate3()
+        {
+            double frequenza = 182, peso = 65, eta = 17, tempo = 30;
+            string RispAsp = "505,0416", sesso = "F";
+            string finalvalue = DataCardio.Esercizi(frequenza, peso, eta, tempo, sesso);
+
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        //Test per verificare le calorie bruciate in una camminata
+        [TestMethod]
+        public void TestCalorieCamminata1()
+        {
+            double peso = 73;
+            double chilometri = 7;
+
+            string RispAsp = "255,5";
+            string finalvalue = DataCardio.MovCamminata(peso, chilometri);
+
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        [TestMethod]
+        public void TestCalorieCamminata2()
+        {
+            double peso = 59.4;
+            double chilometri = 4.6;
+
+            string RispAsp = "136,62";
+            string finalvalue = DataCardio.MovCamminata(peso, chilometri);
+
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        //Test per verificare le calorie bruciate in una corsa
+        [TestMethod]
+        public void TestCalorieCorsa1()
+        {
+            double peso = 78;
+            double chilometri = 5;
+
+            string RispAsp = "351";
+            string finalvalue = DataCardio.MovCorsa(peso, chilometri);
+
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        [TestMethod]
+        public void TestCalorieCorsa2()
+        {
+            double peso = 68.2;
+            double chilometri = 6.9;
+
+            string RispAsp = "423,522";
+            string finalvalue = DataCardio.MovCorsa(peso, chilometri);
+
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        //Test media battiti cardiaci
+        [TestMethod]
+        public void TestMediaBattitiCardiaci1()
+        {
+            int[] BattitiGiornalieri = new int[] { 67, 60, 60, 60, 60, 60, 60, 70, 75, 80, 60, 90, 120, 192, 78, 84, 97, 68, 100, 110, 145, 60, 67, 64 };
+            double finalvalue = DataCardio.MediaBattiti(BattitiGiornalieri);
+
+            double RispAsp = 82;
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        [TestMethod]
+        public void TestMediaBattitiCardiaci2()
+        {
+            int[] BattitiGiornalieri = new int[] { 80, 75, 74, 78, 60, 64, 65, 68, 67, 69, 68, 78, 98, 94, 120, 145, 167, 193, 170, 68, 67, 66, 90, 75 };
+            double finalvalue = DataCardio.MediaBattiti(BattitiGiornalieri);
+
+            double RispAsp = 91;
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        [TestMethod]
+        public void TestMediaBattitiCardiaci3()
+        {
+            int[] BattitiGiornalieri = new int[] { 80, 98, 65, 75, 68, 120, 145, 170, 86, 97, 68, 67, 80, 183, 120, 190, 67, 83, 74, 71, 62, 68, 67, 60 };
+            double finalvalue = DataCardio.MediaBattiti(BattitiGiornalieri);
+
+            double RispAsp = 94;
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        //Test per verificare il battito a riposo
+        [TestMethod]
+        public void TestBattitoRiposo1()
+        {
+            int[] Battiti = new int[] { 95, 60, 61, 94, 72, 60 };
+            double finalvalue = DataCardio.BattitoRiposo(Battiti);
+
+            double RispAsp = 60;
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        [TestMethod]
+        public void TestBattitoRiposo2()
+        {
+            int[] Battiti = new int[] { 120, 87, 94, 82, 73, 76 };
+            double finalvalue = DataCardio.BattitoRiposo(Battiti);
+
+            double RispAsp = 73;
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        //Metodo per verificare la variazione dei battiti
+        [TestMethod]
+        public void TestVariazioneBattito1()
+        {
+            int[] Battiti = new int[] { 120, 87, 94, 82, 73, 76 };
+            double finalvalue = DataCardio.VariazioneBattito(Battiti);
+
+            double RispAsp = 47;
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        [TestMethod]
+        public void TestVariazioneBattito2()
+        {
+            int[] Battiti = new int[] { 193, 75, 68, 81, 91, 84 };
+            double finalvalue = DataCardio.VariazioneBattito(Battiti);
+
+            double RispAsp = 125;
+            Assert.AreEqual(RispAsp, finalvalue);
+        }
+
+        //Test per verificare il riordinamento dei battiti
+        [TestMethod]
+        public void TestRiordinamentoBattiti1()
+        {
+            int[] Battiti = new int[] { 83, 60, 187, 94, 60 };
+            int[] finalvalue = DataCardio.Riordinamento(Battiti);
+
+            int[] RispAsp = Battiti;
+            Assert.AreEqual(RispAsp, finalvalue);
         }
     }
 }
